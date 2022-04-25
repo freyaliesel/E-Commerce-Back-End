fbd5a25c40d88f33caa7c60a18726c66bb149c5d
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-25 15:06:05 -0500

    changed status on a category response

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,7 @@ router.post("/", async (req, res) => {
     // create a new category
     try {
         const categoryData = await Category.create(req.body);
-        res.status(200).json(categoryData);
+        res.status(201).json(categoryData);
     } catch (err) {
         res.status(400).json(err);
     }
