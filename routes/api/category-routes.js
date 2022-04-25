01615d09e2a17b15261cdcc4aa903cce4490f597
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-25 13:04:11 -0500

    get all categories works

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: {model: Product},
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
