d03dce916624f2ef24c38fa9df151b5f64063a10
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-25 14:33:22 -0500

    all tags endpoint works

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+        include: 
+            { model: Product, through: ProductTag, as: "products" },
+    });
+    if (!tagData) {
+        res.status(404).json({
+            message: "No tags found!",
+        });
+        return;
+    }
+    res.status(200).json(tagData);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
@@ -21,8 +36,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+        where: {
+            id: req.params.id,
+        },
+    });
+    if (!tagData) {
+        res.status(404).json({
+            message: "No tag found with this id!",
+        });
+        return;
+    }
+    res.json(tagData);
+} catch (err) {
+    res.json(err);
+}
 });
 
 module.exports = router;
