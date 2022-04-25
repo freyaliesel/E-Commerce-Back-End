c47056ef7406e763f7391014dd999c724a150fd0
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-25 13:36:45 -0500

    adjusted for async

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,9 +24,21 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
+    try {
+        const categoryData = await Category.findByPk(req.params.id);
+        if (!categoryData) {
+            res.status(404).json({
+                message: "No category found with this id!",
+            });
+            return;
+        }
+        res.json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post("/", async (req, res) => {
@@ -45,12 +57,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", async (req, res) => {
     // delete a category by its `id` value
-    const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    return res.json(categoryData)
+    try {
+        const categoryData = await Category.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!categoryData) {
+            res.status(404).json({
+                message: "No category found with this id!",
+            });
+            return;
+        }
+        res.json(categoryData);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 module.exports = router;
